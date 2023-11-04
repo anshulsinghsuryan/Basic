d6eda0972b11bf88cc65f9486d57b65c43a5dd5b
Anshul Singh <anshulsingh55@yahoo.com>
2023-11-04 22:25:41 +0530

    Second Commit

--- src/Basic.java
+++ src/Basic.java
@@ -20,7 +20,7 @@ public class Basic {
 		} else if (bmi >= 18.5 && bmi <= 24.9) {
 			System.out.println("Normal weight " + bmi);
 		} else if (bmi > 24.9 && bmi <= 29.9) {
-			System.out.println("Overweight " + bmi);
+			System.out.println("Overweight -- Thodi exercise to karle bhai" + bmi);
 		} else if (bmi > 30) {
 			System.out.println("Obesity " + bmi);
 		}
