28ccd5740f898bd0e06682826140c85aa2a511e4
Anshul Singh <anshulsingh55@yahoo.com>
2023-11-04 22:21:57 +0530

    first Commit

---
+++ src/Basic.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Basic {
+
+	public static void main(String[] args) {
+
+		// BMI
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter your Height in (cm) ");
+		double height_cm = scan.nextDouble();
+		System.out.println("Please enter your Weight in (KG) ");
+		double weight_kg = scan.nextDouble();
+
+		double mul_height = height_cm / 100 * height_cm / 100;
+
+		double bmi = weight_kg / mul_height;
+
+		if (bmi < 18.5) {
+			System.out.println("Underweight " + bmi);
+		} else if (bmi >= 18.5 && bmi <= 24.9) {
+			System.out.println("Normal weight " + bmi);
+		} else if (bmi > 24.9 && bmi <= 29.9) {
+			System.out.println("Overweight " + bmi);
+		} else if (bmi > 30) {
+			System.out.println("Obesity " + bmi);
+		}
+	}
+}
